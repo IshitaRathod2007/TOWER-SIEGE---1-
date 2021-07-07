6c6c4965a62a00304c5d5e726105111b70224630
IshitaRathod2007 <78262886+IshitaRathod2007@users.noreply.github.com>
2021-07-07 17:30:11 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,25 +1,17 @@
 class Block{
-    constructor(x, y, width, height) {
-        var options = {
-           
-            restitution :0.4,
-            friction :0.0,
-            //isStatic:true
-           
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
       }
-      display(){
-        var angle = this.body.angle;
-        var pos= this.body.position;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0,0,this.width, this.height);
-        pop();
-      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+    }
 }
